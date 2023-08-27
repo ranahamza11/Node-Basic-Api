0c7e0ffc77ef48a42864d17653247e73a45a6053
ranahamza11 <drovisfrovis@gmail.com>
2023-08-27 11:08:07 +0500

    ADD update and delete product option

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const Product = require('./models/productModel')
 const app = express();
 
-
-
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
@@ -12,6 +13,64 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog from youu');
 });
 
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product) {
+            return res.status(404).json({message: `not find the product with id ${id}`});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product) {
+            return res.status(404).json({message: `not find the product with id ${id}`});
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    }catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+})
 
 
 mongoose.connect("mongodb+srv://drovisfrovis:donno123@cluster0.39jyplo.mongodb.net/Node-API?retryWrites=true&w=majority")
